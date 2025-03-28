00e911937a105b1771088d669dc77967d7498416
PB <penguinblackz5@gmail.com>
2025-03-29 00:53:50 +0900

    포커 (수정 3판)

--- cogs/indian.py
+++ cogs/indian.py
@@ -35,7 +35,11 @@ class IndianPoker(commands.Cog):
         bot_hidden_pool.remove(bot_reveal)
         return user_reveal, bot_reveal
 
-    @bot.tree.command(name="인디언포커", description="인디언 포커 게임을 시작합니다.")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.bot.tree.add_command(self.indian_poker)
+
+    @commands.slash_command(name="인디언포커", description="인디언 포커 게임을 시작합니다.")
     async def indian_poker(self, interaction: discord.Interaction, bet_amount: int):
         if bet_amount < 1:
             error_embed = discord.Embed(
